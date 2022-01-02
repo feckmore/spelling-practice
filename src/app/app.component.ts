018bb494442592cc7eb86457e72b785d7f95ab72
David Schultz <dschultz@arthrex.com>
2022-01-02 10:45:39 -0600

    add school 2022 Two Bee list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -310,7 +310,8 @@ export class AppComponent implements OnInit {
 
   resetWordGroups() {
     this.wordGroups = [
-      {name: 'Naomi 2021', selected: true},
+      {name: 'Naomi 2022', selected: true},
+      {name: 'School 2022 Three', selected: false},
       {name: 'School One', selected: false},
       {name: 'School Two', selected: false},
       {name: 'School Three', selected: false},
